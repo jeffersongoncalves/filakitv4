32056624c55b42aa230bcc9ac51dd7c4fc1f9215
Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
2025-03-20 15:17:16 -0300

    build(deps): update dependencies and remove axios setup
    remove `resources/js/bootstrap.js` for axios initialization and add `package-lock.json` to track new dependencies for Tailwind CSS, Vite, and other build tools

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import laravel, { refreshPaths } from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: [
+                'resources/css/filament/admin/theme.css',
+                'resources/css/filament/app/theme.css',
+                'resources/css/filament/public/theme.css',
+                'resources/css/app.css',
+                'resources/js/app.js'
+            ],
+            refresh: [
+                ...refreshPaths,
+                'app/Livewire/**',
+            ],
         }),
-        tailwindcss(),
     ],
 });
